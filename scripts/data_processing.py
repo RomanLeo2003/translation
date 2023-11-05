f65b28d30a9b2f423b1130b1508ff93be2f808a0
User <roma.leontev.03@bk.ru>
2023-11-05 22:05:51 +0300

    data_processing

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -1,5 +1,7 @@
 from tatsoft_api import Translate
 import pandas as pd
+import jsonlines
+import tqdm
 
 tatsoft_translator = Translate()  # tatsoft translation
 
@@ -43,3 +45,35 @@ def form_backtranslated_df(path_to_source_excel, path_to_result_excel):
     result_df.to_excel(path_to_result_excel)
 
 
+def form_duplicated_df(path_to_source_excel, path_to_result_excel, duplicate_scale):
+    source_df = pd.read_excel(path_to_source_excel)
+    rus = source_df['rus'].to_list()
+    tat = source_df['tat'].to_list()
+
+    result_df = pd.DataFrame({'rus': rus * duplicate_scale, 'tat': tat * duplicate_scale})
+    result_df.to_excel(path_to_result_excel)
+
+def excel2json(excel_path, train_json_file, val_json_file, test_json_file, train_val_test_split):
+    df = pd.read_excel(excel_path)
+    df.columns = ['rus', 'tat']
+    df.dropna()
+    train_json_list = []
+    val_json_list = []
+    test_json_list = []
+    for i, row in enumerate(df.iterrows()):
+        if i < train_val_test_split[0]:
+            train_json_list.append({'translation': {'ru': str(row[1]['rus']), 'tat': str(row[1]['tat'])}})
+        elif train_val_test_split[0] <= i < train_val_test_split[1]:
+            val_json_list.append({'translation': {'ru': str(row[1]['rus']), 'tat': str(row[1]['tat'])}})
+        else:
+            test_json_list.append({'translation': {'ru': str(row[1]['rus']), 'tat': str(row[1]['tat'])}})
+
+
+    with jsonlines.open(train_json_file, 'w') as writer:
+        writer.write_all(train_json_list)
+
+    with jsonlines.open(val_json_file, 'w') as writer:
+        writer.write_all(val_json_list)
+
+    with jsonlines.open(test_json_file, 'w') as writer:
+        writer.write_all(test_json_list)
